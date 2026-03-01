bad021a449fd19f4fe9f75f458127007d99bd786
tahir-ameer-dev <tahir.ameer850@gmail.com>
2026-03-01 13:33:16 +0500

    Models and API's are created

---
+++ api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from rest_framework import routers
+from django.conf.urls import include
+from .views import MovieViewSet, RatingViewSet
+router = routers.DefaultRouter()
+router.register('movies', MovieViewSet)
+router.register('ratings', RatingViewSet)
+urlpatterns = [
+    path("", include(router.urls)),
+]
